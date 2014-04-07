de02cbc3cd490ee3e8a519a216bcc83e71dd520b
JJ Allaire <jj@rstudio.org>
2014-04-07 13:11:12 -0400

    add html_escape function

---
+++ R/html_escape.R
@@ -0,0 +1,51 @@
+
+#' Escape HTML entities
+#'
+#' Escape HTML entities contained in a character vector so that it can be safely
+#' included as text or an attribute value within an HTML document
+#'
+#' @param text Text to escape
+#' @param attribute Escape for use as an attribute value
+#'
+#' @return Character vector with escaped text.
+#'
+#' @export
+html_escape <- local({
+
+  .htmlSpecials <- list(
+    `&` = '&amp;',
+    `<` = '&lt;',
+    `>` = '&gt;'
+  )
+  .htmlSpecialsPattern <- paste(names(.htmlSpecials), collapse='|')
+  .htmlSpecialsAttrib <- c(
+    .htmlSpecials,
+    `'` = '&#39;',
+    `"` = '&quot;',
+    `\r` = '&#13;',
+    `\n` = '&#10;'
+  )
+  .htmlSpecialsPatternAttrib <- paste(names(.htmlSpecialsAttrib), collapse='|')
+
+  function(text, attribute=FALSE) {
+    pattern <- if(attribute)
+      .htmlSpecialsPatternAttrib
+    else
+      .htmlSpecialsPattern
+
+    # Short circuit in the common case that there's nothing to escape
+    if (!grepl(pattern, text))
+      return(text)
+
+    specials <- if(attribute)
+      .htmlSpecialsAttrib
+    else
+      .htmlSpecials
+
+    for (chr in names(specials)) {
+      text <- gsub(chr, specials[[chr]], text, fixed=TRUE)
+    }
+
+    return(text)
+  }
+})
