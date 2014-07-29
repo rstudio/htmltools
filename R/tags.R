4a2f71f22c10d421d853f3578ef8ba541fc418e8
Joe Cheng <joe@rstudio.org>
2014-07-29 11:41:42 -0700

    Make reusable function for registering S3 methods

--- R/tags.R
+++ R/tags.R
@@ -1,27 +1,41 @@
 #' @import digest
 NULL
 
-# htmltools provides methods for knitr::knit_print, but knitr isn't a Depends or
-# Imports of htmltools, only an Enhances. Therefore, the NAMESPACE file has to
-# declare it as an export, not an S3method. That means that R will only know to
-# use our methods if htmltools is actually attached, i.e., you have to use
-# library(htmltools) in a knitr document or else you'll get escaped HTML in your
-# document. This code snippet manually registers our methods with S3 once both
-# htmltools and knitr are loaded.
-.onLoad <- function(...) {
-  if ("knitr" %in% loadedNamespaces()) {
-    registerS3method("knit_print", "html", knit_print.html, envir = asNamespace("knitr"))
-    registerS3method("knit_print", "shiny.tag", knit_print.shiny.tag, envir = asNamespace("knitr"))
-    registerS3method("knit_print", "shiny.tag.list", knit_print.shiny.tag.list, envir = asNamespace("knitr"))
-  }
-  setHook(
-    packageEvent("knitr", "onLoad"),
-    function(...) {
-      registerS3method("knit_print", "html", knit_print.html, envir = asNamespace("knitr"))
-      registerS3method("knit_print", "shiny.tag", knit_print.shiny.tag, envir = asNamespace("knitr"))
-      registerS3method("knit_print", "shiny.tag.list", knit_print.shiny.tag.list, envir = asNamespace("knitr"))
+# Reusable function for registering a set of methods with S3 manually. The
+# methods argument is a list of character vectors, each of which has the form
+# c(package, genname, class).
+registerMethods <- function(methods) {
+  lapply(methods, function(method) {
+    pkg <- method[[1]]
+    generic <- method[[2]]
+    class <- method[[3]]
+    func <- get(paste(generic, class, sep="."))
+    if (pkg %in% loadedNamespaces()) {
+      registerS3method(generic, class, func, envir = asNamespace(pkg))
     }
-  )
+    setHook(
+      packageEvent(pkg, "onLoad"),
+      function(...) {
+        registerS3method(generic, class, func, envir = asNamespace(pkg))
+      }
+    )
+  })
+}
+
+.onLoad <- function(...) {
+  # htmltools provides methods for knitr::knit_print, but knitr isn't a Depends or
+  # Imports of htmltools, only an Enhances. Therefore, the NAMESPACE file has to
+  # declare it as an export, not an S3method. That means that R will only know to
+  # use our methods if htmltools is actually attached, i.e., you have to use
+  # library(htmltools) in a knitr document or else you'll get escaped HTML in your
+  # document. This code snippet manually registers our methods with S3 once both
+  # htmltools and knitr are loaded.
+  registerMethods(list(
+    # c(package, genname, class)
+    c("knitr", "knit_print", "html"),
+    c("knitr", "knit_print", "shiny.tag"),
+    c("knitr", "knit_print", "shiny.tag.list")
+  ))
 }
 
 depListToNamedDepList <- function(dependencies) {
