9434e604e1fdd8b332eab188db4509582b97f009
trestletech <jeff.allen@trestletechnology.net>
2019-07-25 09:43:55 -0500

    Don't set when testing locale

--- tests/testthat/helper-locale.R
+++ tests/testthat/helper-locale.R
@@ -1,7 +1,7 @@
 is_locale_available <- function(loc){
   set_locale_failed <- FALSE
   tryCatch(
-    Sys.setlocale("LC_ALL", loc),
+    withr::with_locale(c(LC_COLLATE=loc), {}),
     warning = function(e){ set_locale_failed <<- TRUE }
   )
   !set_locale_failed
