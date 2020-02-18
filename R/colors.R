0d31bad81310779f3a7f7b4a3712a13d231d1d8a
Joe Cheng <joe@rstudio.com>
2020-02-18 08:24:21 -0800

    Implement parseCssColors

--- R/colors.R
+++ R/colors.R
@@ -1,3 +1,118 @@
+#' Parse CSS color strings
+#'
+#' Parses/normalizes CSS color strings, and returns them as strings in
+#' `"#RRGGBB"` and/or `"#RRGGBBAA"` format. Understands hex colors in 3, 4, 6,
+#' and 8 digit forms, `rgb()`/`rgba()`, `hsl()`/`hsla()`, and color keywords.
+#'
+#' @param str CSS color strings
+#' @param mustWork If true, invalid color strings will cause an error; if false,
+#'   then the result will contain `NA` for invalid colors.
+#'
+#' @examples
+#'
+#' parseCssColors(c(
+#'   "#0d6efd",
+#'   "#DC35457F",
+#'   "rgb(32,201,151)",
+#'   "  rgba( 23 , 162 , 184 , 0.5 )  ",
+#'   "hsl(261, 51%, 51%)",
+#'   "cornflowerblue"
+#' ))
+#' @export
+parseCssColors <- function(str, mustWork = TRUE) {
+  # Strip insignificant whitespace
+  str <- color_strip_ws(str)
+
+  strategies <- list(
+    # #RRGGBBAA and #RRGGBB
+    list(
+      pattern = "^#([[:xdigit:]]{2})([[:xdigit:]]{2})([[:xdigit:]]{2})([[:xdigit:]]{2})?$",
+      decoders = list(
+        decode_hex, # red
+        decode_hex, # green
+        decode_hex, # blue
+        decode_optional(decode_hex, 0xFF) # alpha, optional
+      ),
+      encoder = encode_hex
+    ),
+    # #RGBA and #RGB
+    list(
+      pattern = "^#([[:xdigit:]])([[:xdigit:]])([[:xdigit:]])([[:xdigit:]])?$",
+      decoders = list(
+        decode_hex, # red
+        decode_hex, # green
+        decode_hex, # blue
+        decode_optional(decode_hex, 0xFF) # alpha, optional
+      ),
+      encoder = encode_hex
+    ),
+    # rgb() and rgba()
+    list(
+      pattern = paste0("^rgba?\\(",
+        "(", regex_float, "),",
+        "(", regex_float, "),",
+        "(", regex_float, ")",
+        regex_non_capturing_group(",(", regex_float, ")"), "?",
+        "\\)$"),
+      decoders = list(
+        decode_float_255, # red   [0-255]
+        decode_float_255, # green [0-255]
+        decode_float_255, # blue  [0-255]
+        decode_optional(decode_float_1, 0xFF) # alpha [0-1], optional
+      ),
+      encoder = encode_hex
+    ),
+    # hsl() and hsla()
+    list(
+      pattern = paste0("^hsla?\\(",
+        "(", regex_float, "),",
+        "(", regex_float, ")%,",
+        "(", regex_float, ")%",
+        regex_non_capturing_group(",(", regex_float, ")"), "?",
+        "\\)$"),
+      decoders = list(
+        decode_float_identity, # hue        [0-360]
+        decode_float_identity, # saturation [0-255]
+        decode_float_identity, # lightness  [0-255]
+        decode_optional(decode_float_1, 0xFF) # alpha [0-1], optional
+      ),
+      encoder = encode_hsl
+    ),
+    # color keywords
+    list(
+      pattern = paste0("^", regex_color_keyword, "$"),
+      decoders = list(decode_color_keyword),
+      encoder = encode_hex
+    )
+  )
+
+  success <- rep_len(FALSE, length(str))
+  result <- rep_len(NA_character_, length(str))
+
+  for (strat in strategies) {
+    if (all(success)) {
+      break
+    }
+
+    res <- match_and_decode(
+      str[!success],
+      strat$pattern,
+      !!!strat$decoders
+    )
+    if (any(res$matching_rows)) {
+      result[!success][res$matching_rows] <- strat$encoder(res$values)
+      success[!success][res$matching_rows] <- TRUE
+    }
+  }
+
+  if (mustWork && any(!success)) {
+    stop("CSS color value(s) could not be parsed")
+  }
+
+  result
+}
+
+# Strips whitespace that isn't significant in the parsing of CSS colors.
 color_strip_ws <- function(str) {
   str <- gsub("\\s+", " ", str)
   str <- gsub("^ | $", "", str)
@@ -7,9 +122,48 @@ color_strip_ws <- function(str) {
   str
 }
 
+#' @param pattern Regex that contains the same number of capturing groups as
+#'   unnamed arguments in `...`. The capturing groups MUST be non-overlapping.
+#' @param ... Functions for decoding each capturing group
+#' @noRd
+match_and_decode <- function(str, pattern, ...) {
+  args <- rlang::list2(...)
+  m <- regexec(pattern, str)
+  matching_rows <- vapply(m, function(x) { x[1] >= 0 }, logical(1))
+  matches <- regmatches(str[matching_rows], m[matching_rows])
+  if (length(matches) == 0) {
+    return(list(
+      matching_rows = matching_rows
+    ))
+  }
+
+  col_count <- length(matches[[1]])
+  str_matrix <- matrix(unlist(matches), ncol = col_count, byrow = TRUE)[,-1,drop=FALSE]
+  vals <- lapply(seq_len(ncol(str_matrix)), function(i) {
+    args[[i]](str_matrix[,i])
+  })
+  results <- do.call(cbind, vals)
+  return(list(
+    matching_rows = matching_rows,
+    values = results
+  ))
+}
+
+decode_optional <- function(func, default_value) {
+  force(func)
+  force(default_value)
+
+  function(str) {
+    result <- rep_len(default_value, length(str))
+    has_value <- nzchar(str, keepNA = FALSE) & !is.na(str)
+    result[has_value] <- func(str[has_value])
+    result
+  }
+}
+
 # Takes a vector of strings whose elements represent a SINGLE hex color channel
 # (one or two hexadecimal digits) and return an integer in the range [0-255].
-conv_hexstr_to_num255 <- function(str) {
+decode_hex <- function(str) {
   stopifnot(all(nchar(str) %in% c(1,2)))
 
   # Single hex digits get doubled up
@@ -23,15 +177,109 @@ conv_hexstr_to_num255 <- function(str) {
 # Convert strings of floating point numbers [0-255] to integer values in the
 # same range. Valid values outside the range will be clamped. Invalid values
 # will raise errors.
-conv_decstr_to_num255 <- function(str) {
-  stopifnot(all(grepl("^-?[0-9]+(\\.[0-9]+)?$", str) | grepl("^-?\\.[0-9]+$", str)))
-  as.integer(pmax(0, pmin(255, round(as.numeric(str)))))
+decode_float_255 <- function(str) {
+  as.integer(pmax(0, pmin(255, round(decode_float_identity(str)))))
 }
 
 # Convert strings of floating point numbers [0-1] to integer values [0-255].
 # Valid values outside the range will be clamped. Invalid values will raise
 # errors.
-conv_unitstr_to_num255 <- function(str) {
-  stopifnot(all(grepl("^-?[0-9]+(\\.[0-9]+)?$", str) | grepl("^-?\\.[0-9]+$", str)))
-  as.integer(pmax(0, pmin(255, round(as.numeric(str) * 255))))
+decode_float_1 <- function(str) {
+  as.integer(pmax(0, pmin(255, round(decode_float_identity(str) * 255))))
 }
+
+decode_float_identity <- function(str) {
+  stopifnot(all(grepl(paste0("^", regex_float, "$"), str)))
+  as.numeric(str)
+}
+
+encode_hex <- function(values) {
+  if (ncol(values) == 0 || nrow(values) == 0) return(character(0))
+
+  if (ncol(values) < 3) {
+    stop("encode_hex called with too few columns")
+  }
+  if (ncol(values) > 4) {
+    stop("encode_hex called with too many columns")
+  }
+  if (!is.numeric(values) && !is.integer(values)) {
+    stop("encode_hex requires numeric or integer values")
+  }
+
+  if (is.numeric(values)) {
+    values <- round(values)
+  }
+
+  if (any(values > 255) || any(values < 0)) {
+    stop("encode_hex values out of bounds")
+  }
+
+  red <- values[,1]
+  green <- values[,2]
+  blue <- values[,3]
+  alpha <- if (ncol(values) > 3) {
+    values[,4]
+  } else {
+    0xFF
+  }
+  colors <- ifelse(alpha == 0xFF,
+    sprintf("#%02X%02X%02X", red, green, blue),
+    sprintf("#%02X%02X%02X%02X", red, green, blue, alpha)
+  )
+  colors
+}
+
+encode_hsl <- function(values) {
+  if (ncol(values) == 0 || nrow(values) == 0) return(character(0))
+
+  if (ncol(values) < 3) {
+    stop("encode_hsl called with too few columns")
+  }
+  if (ncol(values) > 4) {
+    stop("encode_hsl called with too many columns")
+  }
+  if (!is.numeric(values)) {
+    stop("encode_hsl requires numeric values")
+  }
+
+  H <- values[,1]
+  S <- values[,2] / 100
+  L <- values[,3] / 100
+  alpha <- if (ncol(values) > 3) {
+    values[,4]
+  } else {
+    0xFF
+  }
+
+  # Clamp
+  S <- pmax(0, pmin(1, S))
+  L <- pmax(0, pmin(1, L))
+
+  # https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB_alternative
+  f <- function(n) {
+    k <- (n + H/30) %% 12
+    a <- S * pmin(L, 1-L)
+    L - a * pmax(-1, pmin(k - 3, 9 - k, 1))
+  }
+  rgb <- cbind(f(0), f(8), f(4)) * 255
+  rgb <- cbind(rgb, alpha)
+
+  encode_hex(rgb)
+}
+
+decode_color_keyword <- function(str) {
+  color <- css_color_keywords[str]
+  if (any(is.na(color))) {
+    stop("Invalid color keyword(s)")
+  }
+  t(col2rgb(color, alpha = TRUE))
+}
+
+css_color_keywords <- c(
+  "transparent" = "#00000000",
+  "aliceblue" = "#F0F8FF", "antiquewhite" = "#FAEBD7", "aqua" = "#00FFFF", "aquamarine" = "#7FFFD4", "azure" = "#F0FFFF", "beige" = "#F5F5DC", "bisque" = "#FFE4C4", "black" = "#000000", "blanchedalmond" = "#FFEBCD", "blue" = "#0000FF", "blueviolet" = "#8A2BE2", "brown" = "#A52A2A", "burlywood" = "#DEB887", "cadetblue" = "#5F9EA0", "chartreuse" = "#7FFF00", "chocolate" = "#D2691E", "coral" = "#FF7F50", "cornflowerblue" = "#6495ED", "cornsilk" = "#FFF8DC", "crimson" = "#DC143C", "cyan" = "#00FFFF", "darkblue" = "#00008B", "darkcyan" = "#008B8B", "darkgoldenrod" = "#B8860B", "darkgray" = "#A9A9A9", "darkgreen" = "#006400", "darkgrey" = "#A9A9A9", "darkkhaki" = "#BDB76B", "darkmagenta" = "#8B008B", "darkolivegreen" = "#556B2F", "darkorange" = "#FF8C00", "darkorchid" = "#9932CC", "darkred" = "#8B0000", "darksalmon" = "#E9967A", "darkseagreen" = "#8FBC8F", "darkslateblue" = "#483D8B", "darkslategray" = "#2F4F4F", "darkslategrey" = "#2F4F4F", "darkturquoise" = "#00CED1", "darkviolet" = "#9400D3", "deeppink" = "#FF1493", "deepskyblue" = "#00BFFF", "dimgray" = "#696969", "dimgrey" = "#696969", "dodgerblue" = "#1E90FF", "firebrick" = "#B22222", "floralwhite" = "#FFFAF0", "forestgreen" = "#228B22", "fuchsia" = "#FF00FF", "gainsboro" = "#DCDCDC", "ghostwhite" = "#F8F8FF", "gold" = "#FFD700", "goldenrod" = "#DAA520", "gray" = "#808080", "green" = "#008000", "greenyellow" = "#ADFF2F", "grey" = "#808080", "honeydew" = "#F0FFF0", "hotpink" = "#FF69B4", "indianred" = "#CD5C5C", "indigo" = "#4B0082", "ivory" = "#FFFFF0", "khaki" = "#F0E68C", "lavender" = "#E6E6FA", "lavenderblush" = "#FFF0F5", "lawngreen" = "#7CFC00", "lemonchiffon" = "#FFFACD", "lightblue" = "#ADD8E6", "lightcoral" = "#F08080", "lightcyan" = "#E0FFFF", "lightgoldenrodyellow" = "#FAFAD2", "lightgray" = "#D3D3D3", "lightgreen" = "#90EE90", "lightgrey" = "#D3D3D3", "lightpink" = "#FFB6C1", "lightsalmon" = "#FFA07A", "lightseagreen" = "#20B2AA", "lightskyblue" = "#87CEFA", "lightslategray" = "#778899", "lightslategrey" = "#778899", "lightsteelblue" = "#B0C4DE", "lightyellow" = "#FFFFE0", "lime" = "#00FF00", "limegreen" = "#32CD32", "linen" = "#FAF0E6", "magenta" = "#FF00FF", "maroon" = "#800000", "mediumaquamarine" = "#66CDAA", "mediumblue" = "#0000CD", "mediumorchid" = "#BA55D3", "mediumpurple" = "#9370DB", "mediumseagreen" = "#3CB371", "mediumslateblue" = "#7B68EE", "mediumspringgreen" = "#00FA9A", "mediumturquoise" = "#48D1CC", "mediumvioletred" = "#C71585", "midnightblue" = "#191970", "mintcream" = "#F5FFFA", "mistyrose" = "#FFE4E1", "moccasin" = "#FFE4B5", "navajowhite" = "#FFDEAD", "navy" = "#000080", "oldlace" = "#FDF5E6", "olive" = "#808000", "olivedrab" = "#6B8E23", "orange" = "#FFA500", "orangered" = "#FF4500", "orchid" = "#DA70D6", "palegoldenrod" = "#EEE8AA", "palegreen" = "#98FB98", "paleturquoise" = "#AFEEEE", "palevioletred" = "#DB7093", "papayawhip" = "#FFEFD5", "peachpuff" = "#FFDAB9", "peru" = "#CD853F", "pink" = "#FFC0CB", "plum" = "#DDA0DD", "powderblue" = "#B0E0E6", "purple" = "#800080", "rebeccapurple" = "#663399", "red" = "#FF0000", "rosybrown" = "#BC8F8F", "royalblue" = "#4169E1", "saddlebrown" = "#8B4513", "salmon" = "#FA8072", "sandybrown" = "#F4A460", "seagreen" = "#2E8B57", "seashell" = "#FFF5EE", "sienna" = "#A0522D", "silver" = "#C0C0C0", "skyblue" = "#87CEEB", "slateblue" = "#6A5ACD", "slategray" = "#708090", "slategrey" = "#708090", "snow" = "#FFFAFA", "springgreen" = "#00FF7F", "steelblue" = "#4682B4", "tan" = "#D2B48C", "teal" = "#008080", "thistle" = "#D8BFD8", "tomato" = "#FF6347", "turquoise" = "#40E0D0", "violet" = "#EE82EE", "wheat" = "#F5DEB3", "white" = "#FFFFFF", "whitesmoke" = "#F5F5F5", "yellow" = "#FFFF00", "yellowgreen" = "#9ACD32"
+)
+
+regex_non_capturing_group <- function(...) { paste0("(?:", ..., ")")}
+regex_float <- "[-+]?[0-9]*\\.?[0-9]+"
+regex_color_keyword <- paste0("(", paste0(names(css_color_keywords), collapse = "|"), ")")
--- tests/testthat/test-colors.R
+++ tests/testthat/test-colors.R
@@ -3,68 +3,68 @@ context("test-colors")
 colors_bad <- readLines(test_path("colors-bad.txt"))
 colors_good <- readLines(test_path("colors-bad.txt"))
 
-test_that("conv_hexstr_to_num255", {
-  expect_identical(conv_hexstr_to_num255(sprintf("%02X", 0:255)), 0:255)
-  expect_identical(conv_hexstr_to_num255(sprintf("%02x", 0:255)), 0:255)
-  expect_identical(conv_hexstr_to_num255(sprintf("%1X", 0:15)), as.integer(0:15*16 + 0:15))
-  expect_identical(conv_hexstr_to_num255(sprintf("%1x", 0:15)), as.integer(0:15*16 + 0:15))
+test_that("decode_hex", {
+  expect_identical(decode_hex(sprintf("%02X", 0:255)), 0:255)
+  expect_identical(decode_hex(sprintf("%02x", 0:255)), 0:255)
+  expect_identical(decode_hex(sprintf("%1X", 0:15)), as.integer(0:15*16 + 0:15))
+  expect_identical(decode_hex(sprintf("%1x", 0:15)), as.integer(0:15*16 + 0:15))
 
   # Too many digits
-  expect_error(conv_hexstr_to_num255("000"))
+  expect_error(decode_hex("000"))
   # Too few digits
-  expect_error(conv_hexstr_to_num255(""))
+  expect_error(decode_hex(""))
   # Not a valid number
-  expect_error(conv_hexstr_to_num255("z"))
+  expect_error(decode_hex("z"))
 })
 
-test_that("conv_decstr_to_num255", {
-  expect_identical(conv_decstr_to_num255("-1"), 0L)
-  expect_identical(conv_decstr_to_num255("-0"), 0L)
-  expect_identical(conv_decstr_to_num255("-1.2"), 0L)
-  expect_identical(conv_decstr_to_num255("-.3"), 0L)
-  expect_identical(conv_decstr_to_num255("-.300"), 0L)
-  expect_identical(conv_decstr_to_num255("-300.300"), 0L)
+test_that("decode_float_255", {
+  expect_identical(decode_float_255("-1"), 0L)
+  expect_identical(decode_float_255("-0"), 0L)
+  expect_identical(decode_float_255("-1.2"), 0L)
+  expect_identical(decode_float_255("-.3"), 0L)
+  expect_identical(decode_float_255("-.300"), 0L)
+  expect_identical(decode_float_255("-300.300"), 0L)
 
-  expect_identical(conv_decstr_to_num255("125"), 125L)
-  expect_identical(conv_decstr_to_num255("125.5"), 126L)
+  expect_identical(decode_float_255("125"), 125L)
+  expect_identical(decode_float_255("125.5"), 126L)
   # Rounding goes to nearest even number. :shrug:
-  expect_identical(conv_decstr_to_num255("0.5"), 0L)
-  expect_identical(conv_decstr_to_num255(".5"), 0L)
-  expect_identical(conv_decstr_to_num255("1.5"), 2L)
+  expect_identical(decode_float_255("0.5"), 0L)
+  expect_identical(decode_float_255(".5"), 0L)
+  expect_identical(decode_float_255("1.5"), 2L)
 
-  expect_identical(conv_decstr_to_num255(300), 255L)
+  expect_identical(decode_float_255(300), 255L)
 
-  expect_identical(conv_decstr_to_num255(as.character(0:255)), 0:255)
-  expect_identical(conv_decstr_to_num255(as.character(0:255 + 0.2)), 0:255)
+  expect_identical(decode_float_255(as.character(0:255)), 0:255)
+  expect_identical(decode_float_255(as.character(0:255 + 0.2)), 0:255)
 
-  expect_error(conv_decstr_to_num255("-"))
-  expect_error(conv_decstr_to_num255(""))
-  expect_error(conv_decstr_to_num255("1."))
-  expect_error(conv_decstr_to_num255("."))
-  expect_error(conv_decstr_to_num255(" 1 "))
-  expect_error(conv_decstr_to_num255(" "))
-  expect_error(conv_decstr_to_num255("aa"))
+  expect_error(decode_float_255("-"))
+  expect_error(decode_float_255(""))
+  expect_error(decode_float_255("1."))
+  expect_error(decode_float_255("."))
+  expect_error(decode_float_255(" 1 "))
+  expect_error(decode_float_255(" "))
+  expect_error(decode_float_255("aa"))
 })
 
-test_that("conv_unitstr_to_num255", {
+test_that("decode_float_1", {
   expect_identical(
-    conv_unitstr_to_num255(as.character(0:1000/1000)),
+    decode_float_1(as.character(0:1000/1000)),
     as.integer(round((0:1000/1000)*255))
   )
   # Without leading 0
   expect_identical(
-    conv_unitstr_to_num255(
+    decode_float_1(
       gsub("^0+\\.", ".", as.character(0:1000/1000))
     ),
     as.integer(round((0:1000/1000)*255))
   )
 
   # Clamp
-  expect_identical(conv_unitstr_to_num255(-1), 0L)
-  expect_identical(conv_unitstr_to_num255(2), 255L)
+  expect_identical(decode_float_1(-1), 0L)
+  expect_identical(decode_float_1(2), 255L)
 
-  expect_error(conv_unitstr_to_num255("a"))
-  expect_error(conv_unitstr_to_num255(""))
-  expect_error(conv_unitstr_to_num255(" "))
-  expect_error(conv_unitstr_to_num255(" 0 "))
+  expect_error(decode_float_1("a"))
+  expect_error(decode_float_1(""))
+  expect_error(decode_float_1(" "))
+  expect_error(decode_float_1(" 0 "))
 })
