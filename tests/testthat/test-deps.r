3e800b8b5e5c6883aa656d5ff3844159b52569aa
Carson <cpsievert1@gmail.com>
2020-09-24 17:19:53 -0500

    Improve coverage and DRY up tests

--- tests/testthat/test-deps.r
+++ tests/testthat/test-deps.r
@@ -70,6 +70,12 @@ test_that("Dependency resolution works", {
   expect_warning(subtractDependencies(list(a1.2.1, b1.0.1), list(a1.1)))
 })
 
+expect_html_deps <- function(x, html, deps) {
+  expect_identical(as.character(renderTags(x)$html), html)
+  expect_output(print(as.tags(x)), html)
+  expect_identical(findDependencies(x), deps)
+}
+
 test_that("Inline dependencies", {
   # Test out renderTags and findDependencies when tags are inline
   a1.1 <- htmlDependency("a", "1.1", c(href="/"))
@@ -77,98 +83,79 @@ test_that("Inline dependencies", {
 
   # tagLists ----------------------------------------------------------
   x <- tagList(a1.1, div("foo"), "bar")
-  expect_identical(findDependencies(x), list(a1.1))
-  expect_identical(as.character(renderTags(x)$html), "<div>foo</div>\nbar")
+  expect_html_deps(x, "<div>foo</div>\nbar", list(a1.1))
 
   x <- tagList(a1.1, div("foo"), a1.2, "bar")
-  expect_identical(findDependencies(x), list(a1.1, a1.2))
-  expect_identical(as.character(renderTags(x)$html), "<div>foo</div>\nbar")
+  expect_html_deps(x, "<div>foo</div>\nbar", list(a1.1, a1.2))
 
   x <- tagList(a1.1, div("foo"), "bar", tagFunction(function() { a1.2 }))
-  expect_identical(findDependencies(x), list(a1.1, a1.2))
-  expect_identical(as.character(renderTags(x)$html), "<div>foo</div>\nbar")
+  expect_html_deps(x, "<div>foo</div>\nbar", list(a1.1, a1.2))
 
   x <- tagList(a1.1, div("foo"), "bar", tagFunction(function() { div("baz", a1.2) }))
-  expect_identical(findDependencies(x), list(a1.1, a1.2))
-  expect_identical(as.character(renderTags(x)$html), "<div>foo</div>\nbar\n<div>baz</div>")
+  expect_html_deps(x, "<div>foo</div>\nbar\n<div>baz</div>", list(a1.1, a1.2))
 
   # Mixing inline and attribute dependencies
   x <- attachDependencies(
     tagList(a1.1, div("foo"), "bar"),
     a1.2, append = TRUE
   )
-  expect_identical(findDependencies(x), list(a1.1, a1.2))
-  expect_identical(as.character(renderTags(x)$html), "<div>foo</div>\nbar")
+  expect_html_deps(x, "<div>foo</div>\nbar", list(a1.1, a1.2))
 
   x <- attachDependencies(
     tagList(a1.1, div("foo"), "bar"),
     tagFunction(function() { a1.2 }),
     append = TRUE
   )
-  expect_identical(findDependencies(x), list(a1.1, a1.2))
-  expect_identical(as.character(renderTags(x)$html), "<div>foo</div>\nbar")
+  expect_html_deps(x, "<div>foo</div>\nbar", list(a1.1, a1.2))
 
   x <- attachDependencies(
     tagList(div("foo"), "bar", tagFunction(function() { a1.1 })),
     tagFunction(function() { a1.2 }),
     append = TRUE
   )
-  expect_identical(findDependencies(x), list(a1.1, a1.2))
-  expect_identical(as.character(renderTags(x)$html), "<div>foo</div>\nbar")
+  expect_html_deps(x, "<div>foo</div>\nbar", list(a1.1, a1.2))
 
   # tags with children ------------------------------------------------
   x <- div(a1.1, div("foo"), "bar")
-  expect_identical(findDependencies(x), list(a1.1))
-  expect_identical(as.character(renderTags(x)$html),
-                   "<div>\n  <div>foo</div>\n  bar\n</div>")
+  expect_html_deps(x, "<div>\n  <div>foo</div>\n  bar\n</div>", list(a1.1))
 
   x <- div(
     tagFunction(function() { a1.1 }),
     tagFunction(function() { div("foo") }),
     tagFunction(function() { "bar" })
   )
-  expect_identical(findDependencies(x), list(a1.1))
-  expect_identical(as.character(renderTags(x)$html),
-                   "<div>\n  <div>foo</div>\n  bar\n</div>")
+  expect_html_deps(x, "<div>\n  <div>foo</div>\n  bar\n</div>", list(a1.1))
 
   x <- tagFunction(function() {
     div(div("foo"), a1.2, tagFunction(function() { "bar"}), a1.1)
   })
-  expect_identical(findDependencies(x), list(a1.2, a1.1))
-  expect_identical(as.character(renderTags(x)$html),
-                   "<div>\n  <div>foo</div>\n  bar\n</div>")
+  expect_html_deps(x, "<div>\n  <div>foo</div>\n  bar\n</div>", list(a1.2, a1.1))
 
   x <- attachDependencies(div(a1.1, div("foo"), "bar"), a1.2, append = TRUE)
-  expect_identical(findDependencies(x), list(a1.1, a1.2))
-  expect_identical(as.character(renderTags(x)$html),
-                   "<div>\n  <div>foo</div>\n  bar\n</div>")
+  expect_html_deps(x, "<div>\n  <div>foo</div>\n  bar\n</div>", list(a1.1, a1.2))
 
   x <- attachDependencies(div(a1.1, div("foo"), "bar"), tagFunction(function() { a1.2 }), append = TRUE)
-  expect_identical(findDependencies(x), list(a1.1, a1.2))
-  expect_identical(as.character(renderTags(x)$html),
-                   "<div>\n  <div>foo</div>\n  bar\n</div>")
+  expect_html_deps(x, "<div>\n  <div>foo</div>\n  bar\n</div>", list(a1.1, a1.2))
 
   # Passing normal lists to tagLists and tag functions  ---------------
   x <- tagList(list(a1.1, div("foo")), "bar")
-  expect_identical(findDependencies(x), list(a1.1))
+  expect_html_deps(x, "<div>foo</div>\nbar", list(a1.1))
 
   x <- tagList(list(tagFunction(function() { a1.1 }), div("foo")), "bar")
-  expect_identical(findDependencies(x), list(a1.1))
+  expect_html_deps(x, "<div>foo</div>\nbar", list(a1.1))
 
   x <- div(list(a1.1, div("foo")), "bar")
-  expect_identical(findDependencies(x), list(a1.1))
+  expect_html_deps(x, "<div>\n  <div>foo</div>\n  bar\n</div>", list(a1.1))
 
   x <- div(list(tagFunction(function() { a1.1 }), div("foo")), "bar")
-  expect_identical(findDependencies(x), list(a1.1))
+  expect_html_deps(x, "<div>\n  <div>foo</div>\n  bar\n</div>", list(a1.1))
 
   # Top-level lists -----------------------------------
   x <- list(div("ab"), "cd", a1.1)
-  expect_identical(findDependencies(x), list(a1.1))
-  expect_identical(as.character(renderTags(x)$html), "<div>ab</div>\ncd")
+  expect_html_deps(x, "<div>ab</div>\ncd", list(a1.1))
 
   x <- tagList(tagFunction(function() { list(div("ab"), "cd", a1.1) }), "bar")
-  expect_identical(findDependencies(x), list(a1.1))
-  expect_identical(as.character(renderTags(x)$html), "<div>ab</div>\ncd\nbar")
+  expect_html_deps(x, "<div>ab</div>\ncd\nbar", list(a1.1))
 })
 
 test_that("Modifying children using dependencies", {
