1a89ad8e0b39dd656e28bfd3a2528d8afe60222f
Barret Schloerke <schloerke@gmail.com>
2021-04-02 12:26:30 -0400

    Test selector. Remove :text functionality

--- R/selector.R
+++ R/selector.R
@@ -14,7 +14,6 @@ as_selector <- function(selector) {
   # make sure it's a trimmed string
   selector <- str_trim(selector)
 
-
   # yell if there is a comma
   if (str_detect(selector, ",", fixed = TRUE)) {
     stop("Do not know how to handle comma separated selector values")
@@ -25,20 +24,18 @@ as_selector <- function(selector) {
   }
 
   # if it contains multiple elements, recurse
+  if (str_detect(selector, "* ", fixed = TRUE)) {
+    # we already match on all elements. No need to know about this selector
+    selector <- str_remove(selector, "* ", fixed = TRUE)
+  }
   if (str_detect(selector, " ")) {
-    if (str_detect(selector, "* ", fixed = TRUE)) {
-      stop("Do not know how to handle `*` in the middle of a selector. Please remove it.")
-    }
-
     selector_items <- lapply(strsplit(selector, "\\s+")[[1]], as_selector)
     selector_list <- structure(class = "shiny_selector_list", selector_items)
     return(selector_list)
   }
 
   if (str_detect(selector, ":", fixed = TRUE)) {
-    if (str_detect(selector, ":(?!text$)", perl = TRUE)) {
-      stop("Do not know how to handle special pseudo classes like `:first-child` in selector values other than `:text`")
-    }
+    stop("Do not know how to handle special pseudo classes like `:first-child` or `:text` in selector values")
   }
 
   # https://www.w3.org/TR/selectors-3/#selectors
@@ -48,9 +45,9 @@ as_selector <- function(selector) {
   element <- NULL
   id <- NULL
   classes <- NULL
-  text_children <- FALSE
 
   if (!match_everything) {
+    ## Not needed as the regex values below work around this.
     # # if there is more than a `*`, such as `*.warning`, treat as `.warning`
     # if (str_detect(selector, "^\\*"))
     #   selector <- sub("^\\*", "", selector)
@@ -60,66 +57,34 @@ as_selector <- function(selector) {
     # }
 
     element_regex <- "^[a-zA-Z0-9]+"
-    element <- str_match(selector, element_regex)
+    element <- str_match_first(selector, element_regex)
     if (!is.null(element)) {
       selector <- str_remove(selector, element_regex)
     }
 
-    # # https://www.w3.org/TR/CSS21/syndata.html#value-def-identifier
-    # #  In CSS, identifiers (including element names, classes, and IDs in selectors) can contain only the characters [a-zA-Z0-9] and ISO 10646 characters U+00A0 and higher, plus the hyphen (-) and the underscore (_); they cannot start with a digit, two hyphens, or a hyphen followed by a digit. Identifiers can also contain escaped characters and any ISO 10646 character as a numeric code (see next item). For instance, the identifier "B&W?" may be written as "B\&W\?" or "B\26 W\3F".
+    ## https://www.w3.org/TR/CSS21/syndata.html#value-def-identifier
+    ##  In CSS, identifiers (including element names, classes, and IDs in selectors) can contain only the characters [a-zA-Z0-9] and ISO 10646 characters U+00A0 and higher, plus the hyphen (-) and the underscore (_); they cannot start with a digit, two hyphens, or a hyphen followed by a digit. Identifiers can also contain escaped characters and any ISO 10646 character as a numeric code (see next item). For instance, the identifier "B&W?" may be written as "B\&W\?" or "B\26 W\3F".
     # # define simpler (maybe not accurate) regex
     # id_regex <- "^#[^#.:[\\s]+" # `#` then everything that isn't a `#`, `.`, `:`, or white space
     # class_regex <- "^\\.[^#.:[\\s]+" # `.` then everything that isn't a `.`, `:`, or white space
 
-    # # continuously go over selector for id, class, etc
-    # found <- TRUE
-    # while (found) {
-    #   if (nchar(selector) == 0) {
-    #     break
-    #   }
-    #   # make sure the selector string is shrinking
-    #   found <- FALSE
-    #   prior_size <- cur_size
-
-    #   if (str_detect(selector, id_regex)) {
-    #     if (!is.null(id)) {
-    #       stop("prior id: `", id, "` already found")
-    #     }
-    #     found <- TRUE
-    #     id <- str_match(selector, id_regex)
-    #     selector <- str_remove(selector, id_regex)
-    #   }
-
-    #   if (str_detect(selector, class_regex)) {
-    #     found <- TRUE
-    #     class_val <- str_match(selector, class_regex)
-    #     classes <- c(classes, class_val)
-    #     selector <- str_remove(selector, class_regex)
-    #   }
-    # }
-    # if (found) {
-    #   stop("could not reduce remaining selector: ", selector)
-    # }
-
-    id <- str_remove(str_match(selector, "#[^.:[]+"), "#")
-    if (!is.null(id)) {
-      selector <- str_remove(selector, "#[^.:[]+")
+    tmp_id <- str_match_first(selector, "#[^.:[]+")
+    if (!is.null(tmp_id)) {
+      id <- str_remove(tmp_id, "^#")
+      selector <- str_remove(selector, tmp_id, fixed = TRUE)
     }
 
     classes <- str_remove(str_match_all(selector, "\\.[^.:[]+"), "^\\.")
-    if (!is.null(classes)) {
-      selector <- str_remove(selector, "\\.[^.:[]+")
-    }
-
-    text_children <- str_detect(selector, ":text$")
+    # if (!is.null(classes)) {
+    #   selector <- str_remove(selector, "\\.[^.:[]+")
+    # }
   }
 
   structure(class = "shiny_selector", list(
     element = element,
     id = id,
     classes = classes,
-    match_everything = match_everything,
-    text_children = text_children
+    match_everything = match_everything
   ))
 }
 
@@ -130,15 +95,6 @@ as_selector_list <- function(selector) {
     selector <- structure(class = "shiny_selector_list", list(selector))
   }
 
-  # validate
-  is_text <- which(vapply(selector, `[[`, logical(1), "text_children"))
-  if (length(is_text) > 1) {
-    stop("Only one `:text` pseudo class can be used")
-  }
-  if (length(is_text) == 1 && is_text != length(selector)) {
-    stop("The pseudo class `:text` can only be applied on the last element selection")
-  }
-
   selector
 }
 
@@ -147,7 +103,7 @@ format.shiny_selector <- function(x, ...) {
   if (x$match_everything) {
     paste0("*")
   } else {
-    paste0(x$element, if (!is.null(x$id)) paste0("#", x$id), if (!is.null(x$classes)) paste0(".", x$classes, collapse = ""), if(x$text_children) ":text")
+    paste0(x$element, if (!is.null(x$id)) paste0("#", x$id), if (!is.null(x$classes)) paste0(".", x$classes, collapse = "")
   }
 }
 #' @export
@@ -180,7 +136,7 @@ str_detect <- function(x, pattern, ...) {
 }
 
 # finds first, NOT all
-str_match <- function(x, pattern) {
+str_match_first <- function(x, pattern) {
   reg_info <- regexpr(pattern, x)
   if (length(reg_info) == 1 && reg_info == -1) {
     return(NULL)
---
+++ tests/testthat/test-selector.R
@@ -0,0 +1,94 @@
+
+make_tags <- function(text1, text2) {
+  input_tags <- div(
+    class = "outer",
+    tagList(
+      div(
+        class = "inner",
+        a(href="example.com", "`a` ", list(strong(text1), em(p(text2))), " text."),
+      ),
+      span(
+        class = "sibling",
+        "sibling text"
+      )
+    )
+  )
+
+  input_tags <- span(list(input_tags, input_tags))
+  input_tags
+}
+
+test_that("error checks", {
+  expect_error(as_selector("div, span"), "comma")
+  expect_error(as_selector("div[foo]"), "`[`", fixed = TRUE)
+  expect_error(as_selector("div:text"), "pseudo classes")
+})
+
+
+test_that("selector parses string", {
+  selector <- as_selector("h1#myId-value.class-name .child-class#child-id_value")
+
+  expect_equal(
+    format(selector),
+    "h1#myId-value.class-name #child-id_value.child-class"
+  )
+})
+
+test_that("* checks", {
+  expect_equal(format(as_selector(" * ")), "*")
+  expect_equal(format(as_selector(" *.class-name")), ".class-name")
+  expect_s3_class(as_selector(" * "), "shiny_selector")
+})
+
+
+test_that("* checks", {
+  expect_equal(format(as_selector(" * ")), "*")
+  expect_equal(format(as_selector(" *.class-name")), ".class-name")
+  expect_s3_class(as_selector(" * "), "shiny_selector")
+})
+
+
+
+
+
+# x <- div(class = "foo", "text 1",
+#   div(class = "bar", "text 2"),
+#   div(class = "bar", "text 3",
+#     span("more text")
+#   )
+# )
+# y <- x
+# x <- x[1:length(x)]
+# mutate_in_place(x)
+# x %>% find(".bar", function(item) { toupper(x$children); item })
+# y <- x %>% el_find(".bar") %>% el_find(".x") %>% el_mutate(function(x) toupper(x$children))
+# x %>% find(".bar") %>% addClass("abc")
+# x %>% find(".bar") %>% removeClass("abc")
+# x %>% find(".bar") %>% attr(`data-x` = 123)
+# x %>% find(".bar") %>% css(color = "red")
+# actionButton("x", "X") %>% removeClass("btn-default") %>% addClass("btn-primary")
+# x <- actionButton("x", "X")
+# x$attribs$class <- sub("btn-default", "btn-primary", x$attribs$class)
+# removeClass.shiny.tag <- function(x, class) {
+#   ...
+# }
+# x <- div(
+#   actionButton("x", "X")
+# )
+# # Selection types:
+# #  - tags
+# #  - classes
+# #  - ID
+# #  - descendent   ".btn .x"
+# # Future:
+# #  - direct child ".btn > .x"
+# #
+# # Tag functions:
+# #  - delay execution, wrap them somehow
+# #  - need to know we have a way forward
+# x %>% el_select(".btn") %>% el_remove_class("btn-default") %>% el_add_class("btn-primary")
+# x %>% find(".btn") %>% { . %>% removeClass("btn") %>% addClass("btn-primary") }
+# x$select(".btn")$removeClass("btn-default")$addClass("btn-primary")
+# `$.shiny.tag` <- function(x, name, ...) {
+#   el_fns[[name]](x, ...)
+# }
