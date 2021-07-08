3cb59a734a9f3f852fdd387b801f758eb8971aaf
Carson <cpsievert1@gmail.com>
2021-07-08 09:47:19 -0500

    Relax the required version of rlang (obj_address() won't be released until at least Augutst)

--- R/tag_query.R
+++ R/tag_query.R
@@ -1,7 +1,11 @@
 #' @importFrom fastmap fastmap faststack
-#' @importFrom rlang obj_address
 NULL
 
+# TODO: change to rlang::obj_address() after rlang > 0.4.11 hits CRAN
+# (lionel says he'll keep the alias in rlang for the next few years)
+obj_address <- function(x) {
+  getFromNamespace("sexp_address", "rlang")(x)
+}
 
 # TODO-barret followup PR
 # * onRender(x, fn) - tagFunction(x, fn)
