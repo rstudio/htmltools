140ce1c9e95ddf2cc8f470e8af33f10409923f50
Barret Schloerke <barret@rstudio.com>
2021-07-08 11:13:14 -0400

    Update R/tag_query.R

--- R/tag_query.R
+++ R/tag_query.R
@@ -1,7 +1,7 @@
 #' @importFrom fastmap fastmap faststack
 NULL
 
-# TODO: change to rlang::obj_address() after rlang > 0.4.11 hits CRAN
+# TODO: change to rlang::obj_address() after rlang >= 0.4.12 hits CRAN
 # (lionel says he'll keep the alias in rlang for the next few years)
 obj_address <- function(x) {
   getFromNamespace("sexp_address", "rlang")(x)
