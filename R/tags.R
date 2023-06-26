47055517e6a23f37b47dfaaf386dffbe7918e2df
Garrick Aden-Buie <garrick@adenbuie.com>
2023-06-26 10:58:22 -0400

    Revert `includeHTMLDocument()`
    This reverts commit 4e118f9ccc079e2deccf68f1981a08ed34379bc5.
    
    Revert "fix(inlcudeHTMLDocument): typo"
    
    This reverts commit 31fd79412e1f35e18b143b3f5093b14d3c53249f.
    
    Revert "`devtools::document()` (GitHub Actions)"
    
    This reverts commit 19045107d084e6924dd1dfc7281c4e9b4efd5232.
    
    Revert "feat(includeHTMLDocument): Use `src` when `I(path)`"
    
    This reverts commit a0aef3e72469a1d2387ca62d758d9d06421573ca.
    
    Revert "tests(includeHTMLDocument): Add basic tests"
    
    This reverts commit 105f072f1b2338b883d0e6ef0776038b3c84c971.
    
    Revert "feat: Add `includeHTMLDocument()`"
    
    This reverts commit 559800c7ea5a0e9fb60968630ba698ee29836c8f.
    
    # Conflicts:
    #	R/tags.R

--- R/tags.R
+++ R/tags.R
@@ -1734,11 +1734,6 @@ knit_print.html_dependency <- knit_print.shiny.tag
 #'   use a relative path (the base path being the Shiny application directory),
 #'   not an absolute path.
 #'
-#' @note `includeHTML()` directly inlines the contents of the file, which is only
-#'   suitable for fragments of HTML. To include a full HTML document, such as
-#'   the generated output of an HTML R Markdown or Quarto document, use
-#'   `includeHTMLDocument()`.
-#'
 #' @rdname include
 #' @name include
 #' @aliases includeHTML
@@ -1764,7 +1759,7 @@ detect_html_document <- function(lines) {
   lines <- trimws(lines)
 
   # A complete html document starts with doctype declaration or opening <html>
-  if (!grepl("^(<!DOCTYPE html>|<html)", lines, ignore.case = TRUE)) {
+  if (!grepl("^<!DOCTYPE html>|<html", lines, ignore.case = TRUE)) {
     return(FALSE)
   }
   # and ends by closing the `</html>` tag
@@ -1777,38 +1772,6 @@ detect_html_document <- function(lines) {
   # document should not be treated as an HTML fragment.
   TRUE
 }
-
-#' @note `includeHTMLDocument()` embeds the contents of the file in an
-#'   `<iframe>` element. When `path` is a local file, the contents of the file
-#'   will be embedded in the `<iframe>` using the `srcdoc` attribute, which
-#'   embeds the complete HTML document directly in the page where
-#'   `includeHTMLDocument()` was called. When `path` is detected to be a full
-#'   URL starting with `https://` or is wrapped in `I()`, the `src` attribute of
-#'   the `<iframe>` will be set to the URL.
-#'
-#' @rdname include
-#' @export
-includeHTMLDocument <- function(path, ...) {
-  stopifnot(is.character(path), length(path) == 1)
-
-  iframe_args_default <- list(
-    width = "100%",
-    height = "400px",
-    frameborder = "0"
-  )
-
-  iframe_args <- utils::modifyList(iframe_args_default, dots_list(...))
-
-  if (inherits(path, "AsIs") || grepl("^https?://", path)) {
-    iframe_args$src <- as.character(path)
-  } else {
-    lines <- readLines(path, warn=FALSE, encoding='UTF-8')
-    iframe_args$srcdoc <- paste8(lines, collapse='\n')
-  }
-
-  return(tags$iframe(class = "html-fill-item", !!!iframe_args))
-}
-
 #' @note `includeText` escapes its contents, but does no other processing.
 #'   This means that hard breaks and multiple spaces will be rendered as they
 #'   usually are in HTML: as a single space character. If you are looking for
