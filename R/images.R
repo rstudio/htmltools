5fd07df1798385c4025b0ef0142f0b52975f12a9
Carson <cpsievert1@gmail.com>
2024-03-29 13:58:37 -0500

    Start v0.5.8.1 release candidate

--- R/images.R
+++ R/images.R
@@ -17,7 +17,7 @@
 #' @seealso [plotTag()] saves plots as a self-contained `<img>`
 #'   tag.
 #'
-#' @examples
+#' @examplesIf interactive()
 #' # Default settings
 #' res <- capturePlot(plot(cars))
 #'
